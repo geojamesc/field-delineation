e828e35c066aee230d4a323a9a3114b07a81ad7a
James Crone <james.crone@ed.ac.uk>
2022-03-14 13:57:11 +0000

    added new GsaaToEopatchConfigGpkg dataclass and GPKG2Vector class so as to process vectors held in a geopackage rather than postgis db

--- fd/gsaa_to_eopatch.py
+++ fd/gsaa_to_eopatch.py
@@ -33,13 +33,35 @@ from eolearn.geometry import VectorToRaster
 from .utils import BaseConfig, set_sh_config
 
 
+# @dataclass
+# class GsaaToEopatchConfig(BaseConfig):
+#     database: str
+#     user: str
+#     password: str
+#     host: str
+#     port: str
+#     crs: pyproj.crs
+#     vector_feature: Tuple[FeatureType, str]
+#     extent_feature: Tuple[FeatureType, str]
+#     boundary_feature: Tuple[FeatureType, str]
+#     distance_feature: Tuple[FeatureType, str]
+#     eopatches_folder: str
+#     buffer_poly: int = -10
+#     no_data_value: int = 0
+#     width: int = 1100
+#     height: int = 1100
+#     disk_radius: int = 2
+
+
 @dataclass
-class GsaaToEopatchConfig(BaseConfig):
+class GsaaToEopatchConfigGpkg(BaseConfig):
     database: str
     user: str
     password: str
     host: str
     port: str
+    roi_gpkg_filename: str  # filename of ROI GeoPackage
+    reference_data_gpkg_filename: str  # filename of reference data GeoPackage
     crs: pyproj.crs
     vector_feature: Tuple[FeatureType, str]
     extent_feature: Tuple[FeatureType, str]
@@ -81,6 +103,34 @@ class DB2Vector(EOTask):
                               host=self.host, port=self.port) as con:
             df = gpd.GeoDataFrame.from_postgis(spatial_query, con, geom_col='geom', params=parameters)
             df = df.to_crs(utm_crs).rename(columns={'geom': 'geometry'}).set_geometry('geometry')
+
+        eopatch[self.out_vector] = df
+
+        return eopatch
+
+
+class GPKG2Vector(EOTask):
+    """
+    Reads vectors to EOPatch from a local GeoPackage
+    """
+
+    def __init__(self, fields_gpkg_fn: str, roi_gpkg_fn: str, crs: pyproj.crs, vector_output_feature: Tuple):
+        self.fields_gpkg_fn = fields_gpkg_fn
+        self.roi_gpkg_fn = roi_gpkg_fn
+        self.crs = crs
+        self.out_vector = next(self._parse_features(vector_output_feature)())
+
+    def execute(self, eopatch: EOPatch) -> EOPatch:
+        vec_orig = gpd.read_file(self.fields_gpkg_fn)
+        roi = gpd.read_file(self.roi_gpkg_fn)  # test download.geojson')
+        vec_orig['geometry'] = vec_orig.buffer(0)
+        df = vec_orig
+        # df = vec_orig.clip(roi)
+
+        utm_crs = eopatch.bbox.crs.pyproj_crs()
+        project = pyproj.Transformer.from_proj(utm_crs, df.crs)
+
+        query_bbox = transform(project.transform, eopatch.bbox.geometry)
 
         eopatch[self.out_vector] = df
 
@@ -141,17 +191,26 @@ class Extent2Distance(EOTask):
         return eopatch
 
 
-def get_gsaa_to_eopatch_workflow(config: GsaaToEopatchConfig) -> EOWorkflow:
+def get_gsaa_to_eopatch_workflow(config: GsaaToEopatchConfigGpkg) -> EOWorkflow:
     # set up AWS credentials
     sh_config = set_sh_config(config)
 
     # load patch
-    load_task = LoadTask(path=f's3://{config.bucket_name}/{config.eopatches_folder}', config=sh_config)
+    #load_task = LoadTask(path=f's3://{config.bucket_name}/{config.eopatches_folder}', config=sh_config)
+    load_task = LoadTask(path=f'{config.eopatches_folder}', config=sh_config)
     # add original vectors to patch
-    vec2vec = DB2Vector(database=config.database,
-                        user=config.user, password=config.password,
-                        host=config.host, port=config.port, crs=config.crs,
-                        vector_output_feature=config.vector_feature)
+    # vec2vec = DB2Vector(database=config.database,
+    #                     user=config.user, password=config.password,
+    #                     host=config.host, port=config.port, crs=config.crs,
+    #                     vector_output_feature=config.vector_feature)
+
+    # add original vectors to patch from GeoPackage rather than PostGIS db
+    vec2vec = GPKG2Vector(fields_gpkg_fn=config.reference_data_gpkg_filename,
+                          roi_gpkg_fn=config.roi_gpkg_filename,
+                          crs=config.crs,
+                          vector_output_feature=config.vector_feature
+                          )
+
     # get extent mask from vector
     vec2ras = VectorToRaster(config.vector_feature,
                              config.extent_feature,
@@ -167,7 +226,7 @@ def get_gsaa_to_eopatch_workflow(config: GsaaToEopatchConfig) -> EOWorkflow:
                                config.distance_feature,
                                normalize=True)
     # save new features
-    save_task = SaveTask(path=f's3://{config.bucket_name}/{config.eopatches_folder}',
+    save_task = SaveTask(path=f'{config.eopatches_folder}',
                          features=[config.vector_feature,
                                    config.extent_feature,
                                    config.boundary_feature,
