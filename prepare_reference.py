34781251f09d70fa1a6d57fa213869740f4c7e9f
SFrav <simonfraval@gmail.com>
2022-05-11 17:08:37 +0100

    Prediction buffering for odd and even sized data

--- prepare_reference.py
+++ prepare_reference.py
@@ -2,6 +2,8 @@ import argparse
 import numpy as np
 import fiona
 from shapely.geometry import shape
+#from shapely.validation import is_valid
+from shapely.validation import make_valid
 import matplotlib.pyplot as plt
 import pprint
 
@@ -18,19 +20,22 @@ def filter_reference_data(file_path, file, LU_col, out_file, area_threshold, rat
                 for feature in f:
                         if feature['properties'][LU_col] in land_use:
                             area = np.array([shape(feature['geometry']).area])
-                            #width = np.array([shape(feature['geometry']).width for feature in f])
-                            #height = np.array([shape(feature['geometry']).height for feature in f])
-                            #ratio = np.array(width)/np.array(height)
-                            if area > area_threshold: #and ratio > ratio_threshold:
+                            perimeter = np.array([shape(feature['geometry']).length])
+                            ratio = area/perimeter
+                            #valid = shape(feature['geometry']).is_valid #@try to make valid polygons?
+                            #if not valid:
+                            #    feature['geometry'] = make_valid(feature['geometry'])
+
+                            if area > area_threshold and ratio > ratio_threshold: # and valid:
                                 output.write(feature)
 
             else:
                 for i in range(feature.size):
                     area = np.array([shape(feature['geometry']).area])
-                    #width = np.array([shape(feature['geometry']).width for feature in f])
-                    #height = np.array([shape(feature['geometry']).height for feature in f])
-                    #ratio = np.array(width)/np.array(height)
-                    if area > area_threshold: #and ratio[i] > ratio_threshold:
+                    perimeter = np.array([shape(feature['geometry']).length])
+                    ratio = area/perimeter
+                    #valid = shape(feature['geometry']).is_valid
+                    if area > area_threshold and ratio > ratio_threshold: # and valid:
                         output.write(feature)
 
 
@@ -43,11 +48,14 @@ if __name__ == "__main__":
 #    parser.add_argument("-o", "--output_path", help = "Path to the folder to save the prepared reference data.", required = True)
 #    parser.add_argument("-l", "--land_use", help = "Land use to filter by. Default is crops.", default = ["crops"])
 #    parser.add_argument("-a", "--area_threshold", help = "Area threshold in m2 - whole number)
-#    parser.add_argument("-r", "--ratio_threshold", help = "Ratio threshold. Default is 0.5.", default = 0.5)
+#    parser.add_argument("-r", "--ratio_threshold", help = "Ratio threshold. Remove slithers. Default is 4.")
 #    parser.add_argument("-c", "--column_name", help = "Column name to filter by. Default is LU_Col.", default = "LU_Col")
 
 #    args = parser.parse_args()
-    filter_reference_data('reference-data/', 'fields.gpkg', 'USO21', 'fields.gpkg', area_threshold=2, ratio_threshold=10, land_use = ['TIERRA ARABLE'])
+    LU = ["TIERRA ARABLE", "PASTIZAL", "PASTO ARBUSTIVO", "PASTO CON ARBOLADO", "HUERTA", "OLIVAR", "FRUTAL", "VIÑEDO", "FRUTOS SECOS", "FRUTOS SECOS Y OLIVAR", "ASOC. FRUTAL-FRUTOS SECOS", "ASOC. OLIVAR-FRUTAL", "ASOC. OLIVAR-VIÑEDO", "ASOC. FRUTAL-VIÑEDO", "VIÑA-FRUTOS SECOS", "ZONA CONCENTRADA"]
+    filter_reference_data('reference-data/', 'SIGPAC_Pol_Recinto.shp', 'USO21', 'fields_pasture.gpkg', area_threshold=2, ratio_threshold=4, land_use = LU)
+    
+    #Navarra Spain, all = ["TIERRA ARABLE", "PASTIZAL", "PASTO ARBUSTIVO", "PASTO CON ARBOLADO", "HUERTA", "OLIVAR", "FRUTAL", "VIÑEDO", "FRUTOS SECOS", "INVERNADEROS Y CULTIVOS BAJO PLÁSTICO", "FRUTOS SECOS Y OLIVAR", "ASOC. FRUTAL-FRUTOS SECOS", "ASOC. OLIVAR-FRUTAL", "ASOC. OLIVAR-VIÑEDO", "ASOC. FRUTAL-VIÑEDO", "VIÑA-FRUTOS SECOS", "ZONA CONCENTRADA"]
     
 
 
